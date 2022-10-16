8cb94826e0b941296311c6976ed88a1b68a470ac
Carsten Tschense <carsten.tschense@gmail.com>
2022-10-16 23:44:38 +0200

    minimal reproduction

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,5 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
+import fs from 'fs'
+import path from 'path'
 
 type Data = {
   name: string
@@ -9,5 +11,7 @@ export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  let helloWorld = fs.readFileSync(path.join(process.cwd(), 'assets', 'helloworld.txt'), 'utf8');
+
+  res.status(200).json({ name: helloWorld })
 }
